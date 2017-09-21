cd3965418ff893ed7d8bbd6d5f8215697ecfb69a
henriquefernandez <hriqueft@gmail.com>
2017-09-20 23:25:00 -0300

    controllers compose command

---
+++ tests/Unit/Controllers/StudentControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit\Controllers;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class StudentControllerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
